7b0bb1a5bd184a79cd871cfaa805688e324039c5
Dima Rosmait <d.rosmait@datahow.ch>
2022-08-18 18:32:12 +0200

    Add spinner to RecentExpenses screen

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,5 +1,6 @@
 import { useLayoutEffect, useState } from "react";
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
+import LoadingOverlay from "../components/UI/LoadingOverlay";
 import { useExpenses } from "../store/expenses-count";
 import { getDateMinusDates } from "../utils/date";
 import { getExpenses } from "../utils/http";
@@ -9,12 +10,18 @@ function RecentExpenses() {
   const recentExpenses = expenses.filter(
     (expense) => expense.date >= getDateMinusDates(new Date(), 7)
   );
+  const [isFetching, setIsFetching] = useState(false);
 
   useLayoutEffect(() => {
-    getExpenses().then(setExpenses);
+    setIsFetching(true);
+    getExpenses()
+      .then(setExpenses)
+      .finally(() => setIsFetching(false));
   }, []);
 
-  return (
+  return isFetching ? (
+    <LoadingOverlay />
+  ) : (
     <ExpensesOutput
       expenses={recentExpenses}
       periodName="Last 7 days"
